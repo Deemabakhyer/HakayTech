f98de7739efc6114b4819c60d4fe8ce02e76134d
deemabakhyer <deema.ali100@gmail.com>
2026-02-21 02:02:29 +0300

    added animations for first three blocks + logic

---
+++ Assets/Scripts/CodingBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CodingBlock : MonoBehaviour, IEndDragHandler
+{
+    public string blockAction = "BoilWater"; // Set this in the Inspector for each block
+    private bool isInSolution = false;
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter != null && eventData.pointerEnter.CompareTag("SolutionArea"))
+        {
+            isInSolution = true;
+            Debug.Log(blockAction + " added to sequence.");
+        }
+        else
+        {
+            isInSolution = false;
+        }
+    }
+
+    public bool IsInSolution() => isInSolution;
+}
---
+++ Assets/Scripts/SolutionManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SolutionManager : MonoBehaviour
+{
+    // Drag your blocks from the Hierarchy into this list in the Inspector
+    public List<CodingBlock> activeBlocks;
+    public Animator boyAnimator;
+
+    public void OnSubmitButtonPressed()
+    {
+        Debug.Log("Button was pressed!"); // This will show up in the Console at the bottom
+        foreach (var block in activeBlocks)
+        {
+            if (block.IsInSolution())
+            {
+                ExecuteAction(block.blockAction);
+            }
+        }
+    }
+
+    void ExecuteAction(string action)
+    {
+        switch (action)
+        {
+            case "BoilWater":
+                boyAnimator.SetTrigger("MovePotToFire");
+                break;
+            case "AddCoffee":
+                // Trigger add coffee animation
+                break;
+        }
+    }
+}
